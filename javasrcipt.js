bdcc6e09dc16516628c9784946b92b69d6799df6
Satbali <itsmerahulkumar16@gmail.com>
2023-03-06 07:58:08 +0530

    done it

--- javasrcipt.js
+++ javasrcipt.js
@@ -1,18 +1,29 @@
-var btn = document.getElementById('btn')
-var text = document.getElementById('text')
-var para = document.getElementById('para')
-var demo = document.getElementById('demo')
+const btn = document.getElementById('btn')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
 
+
+// User Credintials can be changed
+const username1 = 'softy_123'
+const email1 = 'softy@gmail.com'
+const password1 = '123'
+
+//Game changer
+var a = 0;
 btn.addEventListener('click', () => {
-    if (text.value == "") {
-        btn.style.animation = 'slide 5s infinite'
-        text.placeholder = "Likkh naaa abbbb"
-    } if (text.value == 'likkh diya!') {
-        btn.style.animation = "";
+    if (username.value == username1 && email.value == email1 && password.value == password1) {
+        console.log("success");
     } else {
-
+        if (a == 0) {
+            btn.style.transform = 'translateX(-110px)'
+            a = 1
+            return false
+        }
+        if (a = 1) {
+            btn.style.transform = 'translateX(110px)'
+            a = 0
+            return false
+        }
     }
 })
-setTimeout(() => {
-    para.style.display = 'block'
-}, 2000);
